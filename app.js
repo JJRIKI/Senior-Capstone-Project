bda5a088467f796f416d62a80027b2c8c5e8071c
Blake Dayman <daymanblake@gmail.com>
2021-03-05 17:53:21 -0800

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+var static = require('node-static');
+var http = require('http');
+
+var port = process.env.PORT;
+var directory = __dirname + '/public';
+
+var file = new static.Server('./public');
+
+// if we arent on heroku then we need to readjust the port and directory information and we know that because the port wont be set
+if(typeof port == 'undefined' || !port){
+    directory = './public';
+    port = 8000;
+}
+
+// set up static web server that will deliver files from the filesystem
+var file = new static.Server(directory);
+
+// construct an http server that gets files from the file server
+var app = http.createServer(function(request,response){
+    request.addListener('end', 
+    function(){
+        file.serve(request,response);
+    }
+    ).resume();
+}
+).listen(port);
+console.log('The server is running');
