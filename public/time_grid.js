78e073b57f4479d35e7c3f3aa6cbc7abf6917499
Blake Dayman <daymanblake@gmail.com>
2021-03-09 01:49:24 -0800

    FullCalendar
    Attempted to render a calendar using React. Failed. Might try to render it using JavaScript instead.

--- public/time_grid.js
+++ public/time_grid.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     var calendarEl = document.getElementById('calendar');
+//     var calendar = new FullCalendar.Calendar(calendarEl, {
+//       initialView: 'dayGridMonth'
+//     });
+//     calendar.render();
+//   });
+
 const e = React.createElement;
 
 class LikeButton extends React.Component {
