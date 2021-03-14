1366cf98a51b8f2bd578f2e3ec08da46b0e675f6
James Bek <jbek@westmont.edu>
2021-03-13 19:39:24 -0800

    moved node_modules and added event handler

--- public/time_grid.js
+++ public/time_grid.js
@@ -19,40 +19,41 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      timeZone: 'PST',
-      initialView: 'timeGridWeek',
-    //   headerToolbar: {
-    //     left: 'prev,next today',
-    //     center: 'title',
-    //     right: 'timeGridWeek,timeGridDay'
-    //   },
-    //   events: 'https://fullcalendar.io/demo-events.json'
+        timeZone: 'PST',
+        initialView: 'timeGridWeek',
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'timeGridWeek,timeGridDay'
+    },
+    events: './events.json'
+
     });
 
-    calendar.render();
-  });
+        calendar.render();
+    });
 
     
 
-  const e = React.createElement;
+    const e = React.createElement;
 
 class LikeButton extends React.Component {
-  constructor(props) {
+    constructor(props) {
     super(props);
     this.state = { liked: false };
   }
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
+    render() {
+        if (this.state.liked) {
+        return 'You liked this.';
+        }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
 }
 
 const domContainer = document.querySelector('#like_button_container');
